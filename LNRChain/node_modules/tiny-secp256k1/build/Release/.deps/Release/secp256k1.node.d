460fe9d07af8d7dec6b6b6430ba0e8c14a24cfc1
Andrew Kamal <kamalandrew55@gmail.com>
2019-11-10 18:16:42 -0500

    remove submodules really quick

--- LNRChain/node_modules/tiny-secp256k1/build/Release/.deps/Release/secp256k1.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/secp256k1.node := rm -rf "Release/secp256k1.node" && cp -af "Release/obj.target/secp256k1.node" "Release/secp256k1.node"
--- LNRChain/node_modules/tiny-secp256k1/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-try {
-  module.exports = require('./native')
-} catch (err) {
-  module.exports = require('./js')
-}
