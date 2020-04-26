61c7dc6c45613d4efd50d30b87bd97841b26e1ce
Andrew Kamal <kamalandrew55@gmail.com>
2020-04-26 10:42:47 -0400

    Create decentralized-internet.js

---
+++ .meteor-configs/decentralized-internet.js
@@ -0,0 +1,9 @@
+// Write your package code here!
+'use strict';
+
+module.exports = {
+  name: require('./package').name
+};
+// Variables exported by this module can be imported by other packages and
+// applications. See decentralized-internet-tests.js for an example of importing.
+export const name = 'decentralized-internet';
